da8c4f91a778264bfdd2b8691ee3c21b5c4f030a
Justin Clark <jclarksummit@gmail.com>
2023-03-17 14:39:36 -0400

    Create boilerplate, add Pet class

--- models.py
+++ models.py
@@ -14,3 +14,42 @@ def connect_db(app):
     app.app_context().push()
     db.app = app
     db.init_app(app)
+
+def Pet(Model.db):
+    """Pet."""
+
+    __tablename__ = "pets"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    name = db.Column(
+        db.String(24),
+        nullable=False
+    )
+
+    species = db.Column(
+        db.String(24),
+        nullable=False
+    )
+
+    photo_url = db.Column(
+        db.Text,
+        nullable=False,
+        default=""
+    )
+
+    # Limit pool of choices
+    age = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    available = db.Column(
+        db.Boolean,
+        nullable=False,
+        default=True
+    )
